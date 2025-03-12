6b5ad3db2dfac591ec3ab7bc6fc71c3c8fb8d8f2
hasaN <mdnauman@proton.me>
2025-03-12 09:03:44 +0530

    changed colorscheme to transparency mode

--- lua/hasan/plugins/colorscheme.lua
+++ lua/hasan/plugins/colorscheme.lua
@@ -4,6 +4,9 @@ return {
 	lazy = false, -- Load it during startup
 	priority = 1000, -- Ensure it loads before other UI-related plugins
 	config = function()
+		require("rose-pine").setup({
+			disable_background = true, -- Enables transparency
+		})
 		vim.cmd.colorscheme("rose-pine") -- Alternative syntax
 	end,
 }
