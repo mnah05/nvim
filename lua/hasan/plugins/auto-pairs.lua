166f7ca08f991c6964c779f52963e6c76d9c7131
MD NAUMAN ATHAR HASAN <cix9ine@MDs-MacBook-Air.local>
2025-05-02 05:28:57 +0530

    autopairs simplified

--- lua/hasan/plugins/auto-pairs.lua
+++ lua/hasan/plugins/auto-pairs.lua
@@ -1,12 +1,5 @@
 return {
-  "windwp/nvim-autopairs",
-  event = "InsertEnter",
-  dependencies = { "hrsh7th/nvim-cmp" },
-  config = function()
-    local autopairs = require("nvim-autopairs")
-    autopairs.setup({ check_ts = true }) -- Enable Treesitter integration
-
-    -- Make autopairs work with nvim-cmp
-    require("cmp").event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
-  end,
+	'windwp/nvim-autopairs',
+	event = "InsertEnter",
+	config = true
 }
