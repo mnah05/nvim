139d585c2997d54e70ed22cab6903c65409ea026
MD NAUMAN ATHAR HASAN <cix9ine@MDs-MacBook-Air.local>
2025-05-02 05:25:59 +0530

    small changes in lsp + none-ls setup

--- lua/hasan/lsp/lspconfig.lua
+++ lua/hasan/lsp/lspconfig.lua
@@ -1,26 +1,22 @@
 return {
 	{
 		"williamboman/mason.nvim",
-		lazy = false,
 		config = function()
 			require("mason").setup()
 		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
 		opts = {
 			auto_install = true,
 		},
 	},
 	{
 		"neovim/nvim-lspconfig",
-		lazy = false,
 		dependencies = { "saghen/blink.cmp" },
 		config = function()
 			local lspconfig = require("lspconfig")
 			local cmp_nvim_lsp = require("blink.cmp")
-
 			local capabilities = cmp_nvim_lsp.get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 			local on_attach = function(client, bufnr)
@@ -41,31 +37,14 @@ return {
 				lua_ls = {},
 				pyright = {},
 				gopls = {},
+				clangd = {},
 				ts_ls = {},
 			}
-
 			for server, config in pairs(servers) do
 				config.capabilities = capabilities
 				config.on_attach = on_attach
 				lspconfig[server].setup(config)
 			end
-
-			-- Special setup for clangd
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-				on_attach = on_attach,
-				cmd = {
-					"clangd",
-					"--background-index",
-					"--clang-tidy",
-					"--completion-style=detailed",
-					"--header-insertion=never",
-				},
-				init_options = {
-					usePlaceholders = true,
-					completeUnimported = true,
-				},
-			})
 		end,
 	},
 }
--- lua/hasan/lsp/none-ls.lua
+++ lua/hasan/lsp/none-ls.lua
@@ -4,24 +4,19 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				-- Prettier handles HTML, CSS, JavaScript, and JSON formatting.
 				null_ls.builtins.formatting.prettier.with({
 					filetypes = { "html", "css", "javascript", "json" },
 				}),
-				-- Stylua for Lua formatting.
 				null_ls.builtins.formatting.stylua.with({
 					extra_args = { "--style={IndentWidth: 4, UseTab: Always}" },
 				}),
-				-- Black for Python formatting.
 				null_ls.builtins.formatting.black,
-				-- Clang-format for C/C++ formatting with 4-space tab.
 				null_ls.builtins.formatting.clang_format.with({
-					extra_args = { "--style=LLVM" }, -- fixed here too!
+					extra_args = { "--style=LLVM" },
 				}),
 			},
 		})
 
-		-- Optional: Map a key to format the current buffer.
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 	end,
 }
