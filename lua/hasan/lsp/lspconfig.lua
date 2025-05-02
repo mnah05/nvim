b4eac728a6ccce92787b782127cf31cfea2f542d
MD NAUMAN ATHAR HASAN <cix9ine@MDs-MacBook-Air.local>
2025-05-02 06:40:51 +0530

    some more minor changes in none-ls and lspconfig

--- lua/hasan/lsp/lspconfig.lua
+++ lua/hasan/lsp/lspconfig.lua
@@ -1,50 +1,34 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = { "saghen/blink.cmp" },
-		config = function()
-			local lspconfig = require("lspconfig")
-			local cmp_nvim_lsp = require("blink.cmp")
-			local capabilities = cmp_nvim_lsp.get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())
+	"neovim/nvim-lspconfig",
+	dependencies = { "saghen/blink.cmp" },
+	config = function()
+		local lspconfig = require("lspconfig")
+		local cmp_nvim_lsp = require("blink.cmp")
+		local capabilities = cmp_nvim_lsp.get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-			local on_attach = function(client, bufnr)
-				local bufopts = { noremap = true, silent = true, buffer = bufnr }
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-				vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, bufopts)
-				vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, bufopts)
-				vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-				vim.keymap.set(
-					"n",
-					"<leader>rn",
-					vim.lsp.buf.rename,
-					vim.tbl_extend("force", { desc = "Smart Rename" }, bufopts)
-				)
-			end
-
-			local servers = {
-				lua_ls = {},
-				pyright = {},
-				gopls = {},
-				clangd = {},
-				ts_ls = {},
-			}
-			for server, config in pairs(servers) do
-				config.capabilities = capabilities
-				config.on_attach = on_attach
-				lspconfig[server].setup(config)
-			end
-		end,
-	},
+		local on_attach = function(_, bufnr)
+			local bufopts = { noremap = true, silent = true, buffer = bufnr }
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", { desc = "Hover" }, bufopts))
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition,
+				vim.tbl_extend("force", { desc = "Go to Definition" }, bufopts))
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references,
+				vim.tbl_extend("force", { desc = "References" }, bufopts))
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action,
+				vim.tbl_extend("force", { desc = "Code Action" }, bufopts))
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename,
+				vim.tbl_extend("force", { desc = "Smart Rename" }, bufopts))
+		end
+		local servers = {
+			lua_ls = {},
+			pyright = {},
+			gopls = {},
+			clangd = {},
+			ts_ls = {},
+		}
+		for server, config in pairs(servers) do
+			config.capabilities = capabilities
+			config.on_attach = on_attach
+			lspconfig[server].setup(config)
+		end
+	end,
 }
--- lua/hasan/lsp/none-ls.lua
+++ lua/hasan/lsp/none-ls.lua
@@ -1,22 +1,37 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.prettier.with({
-					filetypes = { "html", "css", "javascript", "json" },
-				}),
-				null_ls.builtins.formatting.stylua.with({
-					extra_args = { "--style={IndentWidth: 4, UseTab: Always}" },
-				}),
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.clang_format.with({
-					extra_args = { "--style=LLVM" },
-				}),
-			},
-		})
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.prettier.with({
+						filetypes = { "html", "css", "javascript", "json" },
+					}),
+					null_ls.builtins.formatting.stylua.with({
+						extra_args = { "--style={IndentWidth: 4, UseTab: Always}" },
+					}),
+					null_ls.builtins.formatting.black,
+					null_ls.builtins.formatting.clang_format.with({
+						extra_args = { "--style=LLVM" },
+					}),
+				},
+			})
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format with LSP" })
+		end,
+	},
 }
