21ca85a86ed9eca0e90fb1a5521afde2171cb90e
MD NAUMAN ATHAR HASAN <nahasan@MDs-MacBook-Air.local>
2025-09-22 03:30:15 +0530

    some shit

--- lua/hasan/plugins/color.lua
+++ lua/hasan/plugins/color.lua
@@ -1,14 +1,14 @@
 return {
-  "folke/tokyonight.nvim",
-  name = "tokyonight",
-  config = function()
-    require("tokyonight").setup({
-      -- transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    })
-    vim.cmd.colorscheme("tokyonight")
-  end,
+	"folke/tokyonight.nvim",
+	name = "tokyonight",
+	config = function()
+		require("tokyonight").setup({
+			-- transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		})
+		vim.cmd.colorscheme("tokyonight")
+	end,
 }
