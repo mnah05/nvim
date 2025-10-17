6e42c0f1b6d62fb17e2eb6aa6b29e2f6fc087bec
MD NAUMAN ATHAR HASAN <mnah05@MDs-MacBook-Air.local>
2025-10-18 01:49:46 +0530

    some changes

--- lua/hasan/plugins/color.lua
+++ lua/hasan/plugins/color.lua
@@ -1,7 +1,7 @@
-return {
-	"rebelot/kanagawa.nvim",
-	config = function(_, opts)
-		require("kanagawa").setup(opts)
-		vim.cmd.colorscheme("kanagawa")
-	end,
+return{
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function ()
+		vim.cmd("colorscheme rose-pine")
+	end
 }
