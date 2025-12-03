cca40714446fe73840f3c0679b727f112bb945d5
MD NAUMAN ATHAR HASAN <mnah05@MDs-MacBook-Air.local>
2025-12-04 04:01:34 +0530

    some bullcrap

---
+++ lua/hasan/plugins/neo-tree.lua
@@ -0,0 +1,29 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	cmd = "Neotree",
+	keys = {
+		{ "<leader>fe", "<cmd>Neotree toggle<cr>", desc = "Explorer" },
+		{ "<leader>fo", "<cmd>Neotree focus<cr>", desc = "Focus Explorer" },
+	},
+	opts = {
+		filesystem = {
+			filtered_items = {
+				hide_dotfiles = false,
+				hide_gitignored = false,
+			},
+		},
+		window = {
+			width = 30,
+			mappings = {
+				["<space>"] = "toggle_node",
+				["<cr>"] = "open",
+			},
+		},
+	},
+}
