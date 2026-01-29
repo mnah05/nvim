b48325f40505dd02c1c6ab555cf6be6762037e4e
MD NAUMAN ATHAR HASAN <mnah05@MDs-MacBook-Air.local>
2026-01-29 19:54:11 +0530

    some backup

--- lua/hasan/plugins/color.lua
+++ lua/hasan/plugins/color.lua
@@ -1,7 +1,16 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		vim.cmd("colorscheme rose-pine")
-	end,
+	{
+		"sainnhe/everforest",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- Option is "hard", "medium" (default) or "soft"
+			vim.g.everforest_background = "soft"
+			vim.opt.background = "dark"
+			-- For better performance
+			vim.g.everforest_enable_italic = 1
+
+			vim.cmd([[colorscheme everforest]])
+		end,
+	},
 }
--- lua/hasan/plugins/nvim-treesitter.lua
+++ lua/hasan/plugins/nvim-treesitter.lua
@@ -1,15 +1,8 @@
--- in your plugin list (e.g. plugins.lua)
-return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		lazy = false, -- or true + proper event if you want lazy-loading
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "lua", "python", "go" },
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-		end,
-	},
+return{
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  opts = {
+    ensure_installed = { "lua", "python", "go" },
+    highlight = { enable = true },
+  },
 }
