07ba5f39199469ad81cc2219d6c36d4d3c204c47
mnah05 <workwithnauman@gmail.com>
2026-02-26 16:34:52 +0530

    26-Feb-2026

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,48 +1,48 @@
 return {
-    -- Mason: LSP installer
-    {
-        "williamboman/mason.nvim",
-        opts = {},
-    },
+	-- Mason: LSP installer
+	{
+		"williamboman/mason.nvim",
+		opts = {},
+	},
 
-    -- Mason-lspconfig bridge
-    {
-        "williamboman/mason-lspconfig.nvim",
-        dependencies = { "williamboman/mason.nvim" },
-        opts = {
-            ensure_installed = {
-                "ts_ls",        -- JS/TS
-                "pyright",      -- Python
-                "lua_ls",       -- Lua
-                "clangd",       -- C/C++
-                "gopls",        -- Go
-            },
-            automatic_installation = true,
-        },
-    },
+	-- Mason-lspconfig bridge
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		opts = {
+			ensure_installed = {
+				"ts_ls", -- JS/TS
+				"pyright", -- Python
+				"lua_ls", -- Lua
+				"clangd", -- C/C++
+				"gopls", -- Go
+			},
+			automatic_installation = true,
+		},
+	},
 
-    -- LSP Config
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "williamboman/mason-lspconfig.nvim",
-            "saghen/blink.cmp",
-        },
-        config = function()
-            local capabilities = require("blink.cmp").get_lsp_capabilities()
+	-- LSP Config
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			"saghen/blink.cmp",
+		},
+		config = function()
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-            local servers = { "ts_ls", "pyright", "lua_ls", "clangd", "gopls" }
-            for _, server in ipairs(servers) do
-                vim.lsp.config(server, { capabilities = capabilities })
-            end
-            vim.lsp.enable(servers)
+			local servers = { "ts_ls", "pyright", "lua_ls", "clangd", "gopls" }
+			for _, server in ipairs(servers) do
+				vim.lsp.config(server, { capabilities = capabilities })
+			end
+			vim.lsp.enable(servers)
 
-            vim.diagnostic.config({
-                virtual_text = true,
-                signs = true,
-                underline = true,
-                update_in_insert = false,
-            })
-        end,
-    },
+			vim.diagnostic.config({
+				virtual_text = true,
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+			})
+		end,
+	},
 }
